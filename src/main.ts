44d852638c59f2817acb90a9401b239b6b0b1da0
Maxi <maxi.gatto96@gmail.com>
2024-09-28 18:49:02 -0300

    feat: add hello world

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,8 @@ async function bootstrap() {
   app.useGlobalFilters(new RpcCustomExceptionFilter());
   await app.listen(envs.port);
 
+  console.log('Hello World!');
+
   logger.log(`Gateway running on port ${envs.port}`);
 }
 bootstrap();
